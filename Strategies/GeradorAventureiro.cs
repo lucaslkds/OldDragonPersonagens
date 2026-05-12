62285a19fe21a11f5a30c7d4939021e6462e0f20
lucaslkds <91992988+lucaslkds@users.noreply.github.com>
2026-05-12 20:25:27 -0300

    Add files via upload

--- Strategies/GeradorAventureiro.cs
+++ Strategies/GeradorAventureiro.cs
@@ -5,36 +5,22 @@ namespace OldDragonPersonagens.Strategies;
 
 public class GeradorAventureiro : IEstrategiaAtributos
 {
-    private readonly DadoService _dadoService = new();
+    public string Nome => "Aventureiro";
 
-    public string Nome => "Estilo Aventureiro";
-    public string Descricao => "Rola 3d6 seis vezes e permite distribuir os valores livremente entre os atributos.";
+    public string Descricao => "Rola 3d6 seis vezes e permite distribuir os valores.";
 
     public Atributos GerarAtributos()
     {
-        List<int> valores = new();
+        List<int> valores = new List<int>();
 
         for (int i = 0; i < 6; i++)
         {
-            valores.Add(_dadoService.Rolar3D6());
+            valores.Add(DadoService.Rolar3D6());
         }
 
-        Console.WriteLine();
-        Console.WriteLine("Distribua os valores gerados entre os atributos.");
+        Console.WriteLine("\nValores gerados no modo Aventureiro:");
+        Console.WriteLine(string.Join(", ", valores));
 
-        return DistribuirManualmente(valores);
-    }
-
-    private static Atributos DistribuirManualmente(List<int> valores)
-    {
-        return new Atributos
-        {
-            Forca = EntradaService.EscolherValorDaLista(valores, "Força"),
-            Destreza = EntradaService.EscolherValorDaLista(valores, "Destreza"),
-            Constituicao = EntradaService.EscolherValorDaLista(valores, "Constituição"),
-            Inteligencia = EntradaService.EscolherValorDaLista(valores, "Inteligência"),
-            Sabedoria = EntradaService.EscolherValorDaLista(valores, "Sabedoria"),
-            Carisma = EntradaService.EscolherValorDaLista(valores, "Carisma")
-        };
+        return DistribuidorAtributosService.Distribuir(valores);
     }
 }
--- Strategies/GeradorHeroico.cs
+++ Strategies/GeradorHeroico.cs
@@ -5,36 +5,22 @@ namespace OldDragonPersonagens.Strategies;
 
 public class GeradorHeroico : IEstrategiaAtributos
 {
-    private readonly DadoService _dadoService = new();
+    public string Nome => "Heroico";
 
-    public string Nome => "Estilo Heroico";
-    public string Descricao => "Rola 4d6, descarta o menor dado, soma os outros três e permite distribuir os valores livremente.";
+    public string Descricao => "Rola 4d6, descarta o menor dado e permite distribuir os valores.";
 
     public Atributos GerarAtributos()
     {
-        List<int> valores = new();
+        List<int> valores = new List<int>();
 
         for (int i = 0; i < 6; i++)
         {
-            valores.Add(_dadoService.Rolar4D6DescartandoMenor());
+            valores.Add(DadoService.Rolar4D6DescartandoMenor());
         }
 
-        Console.WriteLine();
-        Console.WriteLine("Distribua os valores gerados entre os atributos.");
+        Console.WriteLine("\nValores gerados no modo Heroico:");
+        Console.WriteLine(string.Join(", ", valores));
 
-        return DistribuirManualmente(valores);
-    }
-
-    private static Atributos DistribuirManualmente(List<int> valores)
-    {
-        return new Atributos
-        {
-            Forca = EntradaService.EscolherValorDaLista(valores, "Força"),
-            Destreza = EntradaService.EscolherValorDaLista(valores, "Destreza"),
-            Constituicao = EntradaService.EscolherValorDaLista(valores, "Constituição"),
-            Inteligencia = EntradaService.EscolherValorDaLista(valores, "Inteligência"),
-            Sabedoria = EntradaService.EscolherValorDaLista(valores, "Sabedoria"),
-            Carisma = EntradaService.EscolherValorDaLista(valores, "Carisma")
-        };
+        return DistribuidorAtributosService.Distribuir(valores);
     }
 }
